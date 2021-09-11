175e87a67a62b98ea585586b29c58de5e2373167
JPerez <imbajoshuaperez@gmail.com>
2021-09-11 12:55:02 +0800

    Edit page

---
+++ README.md
@@ -0,0 +1,23 @@
+# Grocery List
+
+## Table of contents
+
+- [General Info](#general-information)
+- [Technologies](#technologies)
+- [Credits](#credits)
+
+## General Info
+
+This is a simple application where you can list all stuff you want to buy.
+
+## Technologies
+
+Project is created with
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Credits
+
+<a target="_blank" href="https://icons8.com/icon/sWs85cY1QuC7/list">List</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- script.js
+++ script.js
@@ -13,7 +13,23 @@ function addItem() {
     else {
         myList.appendChild(item);
         document.getElementById("input").value = "";
-    }   
+    }
+    
+    let span = document.createElement("span");
+    let i = document.createElement("i");
+    span.className = "close";
+    i.className = "fas fa-minus-circle";
+    span.appendChild(i);
+    item.appendChild(span);
+
+    let close = document.getElementsByClassName("close");
+    for(let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        let li = this.parentElement;
+        myList.removeChild(li);
+      }
+    }
+
 }
 let list = document.getElementById('myList');
 list.addEventListener('click', function(ev) {
